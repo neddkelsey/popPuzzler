0fde73f66ead0213bdbc9795d311377b2d5a42a5
neddkelsey <kan54@cornell.edu>
2023-10-24 12:18:27 -0700

    first commit for popPuzzle

---
+++ src/main/java/com/popPuzzler/PopPuzzlerApplication.java
@@ -0,0 +1,13 @@
+package com.popPuzzler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopPuzzlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopPuzzlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/popPuzzler/PopPuzzlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popPuzzler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopPuzzlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
